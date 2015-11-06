7185541301b5c01297990f82fa43d42a8d6562f4
Frans Twisk <frans@franstwisk.nl>
2015-11-06 14:59:17 +0100

    API gefixed en kleine verbeteringen

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 Meteor.publish('projects', function() {
     var _this = this;
     try {
-        var response = HTTP.get('https://www.onepercentclub.com/api/bb_projects/previews/?ordering=popularity&page=1&page_size=3&_=1446812641378');
+        var response = HTTP.get('https://www.onepercentclub.com/api/bb_projects/previews/?ordering=-amount_donated&page=1&page_size=3&_=1446812641378');
         _.each(response.data.results, function(shot) {
             var projectData = {
                 project_name: shot.title,
